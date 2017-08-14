177b1241c0c91a670d35c0dad005f1a4571b3226
Bryan Pellegrim <Bryyan94@live.com>
2017-08-13 22:19:04 -0700

    Test For Additional Pass Screen
    Addition of a password screen for the inventory

---
+++ Password.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace CST_117___Inventory_Project
+{
+    partial class Password
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.LogInButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inventoryPasswordSQLdocDataSet = new CST_117___Inventory_Project.InventoryPasswordSQLdocDataSet();
+            this.passwordListBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.passwordListTableAdapter = new CST_117___Inventory_Project.InventoryPasswordSQLdocDataSetTableAdapters.PasswordListTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryPasswordSQLdocDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordListBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(395, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 66);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 59);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 59);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(395, 362);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(377, 66);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // LogInButton
+            // 
+            this.LogInButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogInButton.Location = new System.Drawing.Point(91, 461);
+            this.LogInButton.Name = "LogInButton";
+            this.LogInButton.Size = new System.Drawing.Size(681, 71);
+            this.LogInButton.TabIndex = 4;
+            this.LogInButton.Text = "Log In";
+            this.LogInButton.UseVisualStyleBackColor = true;
+            this.LogInButton.Click += new System.EventHandler(this.LogInButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(267, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 59);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Please Log In";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(350, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(195, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Employees Only!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 583);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "<Back>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inventoryPasswordSQLdocDataSet
+            // 
+            this.inventoryPasswordSQLdocDataSet.DataSetName = "InventoryPasswordSQLdocDataSet";
+            this.inventoryPasswordSQLdocDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // passwordListBindingSource
+            // 
+            this.passwordListBindingSource.DataMember = "PasswordList";
+            this.passwordListBindingSource.DataSource = this.inventoryPasswordSQLdocDataSet;
+            // 
+            // passwordListTableAdapter
+            // 
+            this.passwordListTableAdapter.ClearBeforeFill = true;
+            // 
+            // Password
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 644);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LogInButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Password";
+            this.Text = "Employee Log-In";
+            this.Load += new System.EventHandler(this.Password_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryPasswordSQLdocDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordListBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button LogInButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private InventoryPasswordSQLdocDataSet inventoryPasswordSQLdocDataSet;
+        private System.Windows.Forms.BindingSource passwordListBindingSource;
+        private InventoryPasswordSQLdocDataSetTableAdapters.PasswordListTableAdapter passwordListTableAdapter;
+    }
+}
